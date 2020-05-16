55cc8cdd85532a993a7fd4985976e03d82aef30a
sanyamdtu <sanyamexam@gmail.com>
2020-05-16 15:46:45 +0530

    Enter

--- codeforces/#640/special_permutations.cpp
+++ codeforces/#640/special_permutations.cpp
@@ -1,59 +1,72 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define forn(i,n) for(int i=0;i<n;i++)
-void dfs(vector<int> &v,int i,int *visited,int **edge,int n){
-    if(!visited[i]){
-        v.push_back(i);
-        visited[i]=1;
-        for(int j=n-1;j>=0;j--){
-            if(edge[i][j]==1&& visited[j]!=1){
-                dfs(v,j,visited,edge,n);
-            }
-        }
-    }
-}
+// void dfs(vector<int> &v,int i,int *visited,int **edge,int n){
+//     if(!visited[i]){
+//         v.push_back(i);
+//         visited[i]=1;
+//         for(int j=n-1;j>=0;j--){
+//             if(edge[i][j]==1&& visited[j]!=1){
+//                 dfs(v,j,visited,edge,n);
+//             }
+//         }
+//     }
+// }
 int main(){
     int t;
     cin>>t;
     while(t--){
-        int n;
+    int n;
     cin>>n;
-    int **edge=new int *[n];
-    forn(i,n)
-        edge[i]=new int [n];
-    forn(i,n)
-    forn(j,n)
-    edge[i][j]=0;
-    for(int i=0;i<n;i++){
-        if(i+2<n){
-            edge[i][i+2]=1;
-            edge[i+2][i]=1;
-            if(i+3<n){
-                edge[i][i+3]=1;
-                edge[i+3][i]=1;
-                if(i+4<n){
-                    edge[i][i+4]=1;
-                    edge[i+4][i]=1;
-                }
-            }   
-        }        
-    }
+    // int **edge=new int *[n];
+    // forn(i,n)
+    //     edge[i]=new int [n];
+    // forn(i,n)
+    // forn(j,n)
+    // edge[i][j]=0;
+    // for(int i=0;i<n;i++){
+    //     if(i+2<n){
+    //         edge[i][i+2]=1;
+    //         edge[i+2][i]=1;
+    //         if(i+3<n){
+    //             edge[i][i+3]=1;
+    //             edge[i+3][i]=1;
+    //             if(i+4<n){
+    //                 edge[i][i+4]=1;
+    //                 edge[i+4][i]=1;
+    //             }
+    //         }   
+    //     }        
+    // }
     
-    int *visited=new int [n];
-    forn(i,n)
-     visited[i]=0;
-    vector<int> ans;
-    dfs(ans,0,visited,edge,n);
+    // int *visited=new int [n];
+    // forn(i,n)
+    //  visited[i]=0;
+    // vector<int> ans;
+    // dfs(ans,0,visited,edge,n);
     
-    if(ans.size()==n){
-        cout<<ans[n-1]+1<<" ";
-        forn(i,ans.size()-1)
-        cout<<ans[i]+1<<" ";
-        cout<<endl;
-    }
-    else
-     cout<<-1<<endl;
-    }
+    // if(ans.size()==n){
+    //     cout<<ans[n-1]+1<<" ";
+    //     forn(i,ans.size()-1)
+    //     cout<<ans[i]+1<<" ";
+    //     cout<<endl;
+    // }
+    // else
+    //  cout<<-1<<endl;
+    //
+            if(n<4)
+             cout<<-1<<endl;
+            else{
+                for(int i=1;i<n-3;i+=2)
+                 cout<<i<<" ";
+                cout<<n-1<<" "<<n-3<<" "<<n<<" "<<n-2<<" ";
+                int j;
+                (n%2==0)?j=n-4:j=n-5;
+                for(;j>=2;j-=2)
+                 cout<<j<<" ";
+                cout<<endl;
+             }
+    }    
 }
 //1 2 3 4 5 6 7 8 9 10
 //1 3 2 5 4 7 6 9 8 10
