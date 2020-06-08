0e8188c00d759c33783ac588dc9558d2652719db
sanyamdtu <sanyamexam@gmail.com>
2020-06-08 23:08:57 +0530

    dp2 ongoing

--- practice_codes/DP_2/square_brackets.cpp
+++ practice_codes/DP_2/square_brackets.cpp
@@ -47,10 +47,44 @@ Sample Output
 2 
 3 
 2 
+
+input
+10
+8 4
+4 11 8 10 
+8 5
+2 12 4 8 5 
+7 3
+7 11 1 
+7 6
+6 9 7 1 8 5 
+7 5
+12 9 6 1 13 
+7 4
+3 1 9 6 
+8 3
+11 2 12 
+6 4
+1 6 9 2 
+7 5
+7 3 8 10 1 
+7 5
+13 7 5 2 12
+output
+44
+33
+85
+2
+0
+61
+146
+15
+18
+0
 */
 #include <bits/stdc++.h>
 using namespace std;
-int recursive(int sum, int i, int n, int k, queue<int> q)
+int recursive(int sum, int i, int n, int k, queue<int> q, int dp[][100])
 {
     if (i == n)
     {
@@ -58,23 +92,29 @@ int recursive(int sum, int i, int n, int k, queue<int> q)
             return 1;
         return 0;
     }
-    if (sum < 0)
+    else if (sum < 0)
         return 0;
-    if (!q.empty() && i == q.front() - 1)
+    else if (dp[i][sum] >= 0)
+        return dp[i][sum];
+    else if (!q.empty() && i == q.front() - 1)
     {
         q.pop();
-        return recursive(sum + 1, i + 1, n, k, q);
+        dp[i][sum] = recursive(sum + 1, i + 1, n, k, q, dp);
+        return dp[i][sum];
     }
 
     else
-        return recursive(sum - 1, i + 1, n, k, q) + recursive(sum + 1, i + 1, n, k, q);
+        dp[i][sum] = recursive(sum - 1, i + 1, n, k, q, dp) + recursive(sum + 1, i + 1, n, k, q, dp);
+    return dp[i][sum];
 }
 int main()
 {
+    int dp[100][100];
     int t;
     cin >> t;
     while (t--)
     {
+        vector<int> v;
         int n, k;
         cin >> n >> k;
         queue<int> q;
@@ -82,9 +122,19 @@ int main()
         {
             int temp;
             cin >> temp;
-            q.push(temp);
+            v.push_back(temp);
         }
-        cout << recursive(0, 0, 2 * n, k, q) << endl;
+        sort(v.begin(), v.end());
+        memset(dp, -1, sizeof(dp));
+        for (int i = 0; i < k; i++)
+            q.push(v[i]);
+        // cout << recursive(0, 0, 2 * n, k, q, dp) << endl;
+        // for (int i = 0; i < 2 * n; i++)
+        // {
+        //     for (int j = 0; j < n; j++)
+        //         cout << dp[i][j] << " ";
+        //     cout << endl;
+        // }
     }
     return 0;
 }
--- practice_codes/DP_2/tempCodeRunnerFile.cpp
+++
@@ -1,2 +0,0 @@
-//b = rev(b);
-        //ll sum1 = sum(a, rev(a));
